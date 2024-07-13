6e734c03deec9b541b83dce9a7cfb836a4f72b74
Udreck <narongrit_st@tni.ac.th>
2024-07-13 11:34:43 +0700

    Componant

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View , Button, Alert} from 'react-native';
+import AppFooter from './componant/AppFooter';
+import AppHeader from './componant/AppHeader';
 
-export default function App() {
+export default function App():React.JSX.Element {
+  const onClickMe = ()=>{
+    Alert.alert("Hi","hello React.jf");
+  }
+  const users =[{id:1001, name:'John'}, {id:1002, name:'Marry'}]
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AppHeader title="This is Header1" year={1999}/>
+      <AppHeader title="This is Header2"/>
+      <Text>Hello React Native!</Text>
+      {users.map((data, index)=>{
+        return(
+          <Text key={data.id}>
+            No.{index+1} ID: {data.id} Name: {data.name}
+          </Text>
+        )
+      })}
+      {/* <Button title="Click Me" onPress={()=>{Alert.alert('Hi','React Native is Fuck!!')}}/> */}
+      <Button title="Click Me" onPress={onClickMe} color="red"/>
+      <AppFooter />
     </View>
   );
 }
