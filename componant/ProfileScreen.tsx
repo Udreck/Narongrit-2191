8558579d801bca151c0c45d038c8cfeae97c0f77
Udreck <narongrit_st@tni.ac.th>
2024-07-27 11:52:43 +0700

    endW4

--- componant/ProfileScreen.tsx
+++ componant/ProfileScreen.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native'
+import React, { useState } from 'react'
 import styles from '../styles/styles';
 
 const ProfileScreen = ():React.JSX.Element => {
-    const profileImage = require("../assets/lo.jpg");
+    const [profileImage,setImage] = useState(require("../assets/lo.jpg"));
+    const [name,setName] = useState("Narongrit Prommadaj");
+    const handleChangeName = ()=>{
+        setName(name == "Narongrit Prommadaj" ? "Avada Kedubbla" : "Narongrit Prommadaj");
+    }
+    const handleChangeImahe = ()=>{
+        //setImage(require("../assets/ma.jpg"));
+        setImage(profileImage == require("../assets/lo.jpg") ? require("../assets/ma.jpg"): require("../assets/lo.jpg"));
+    }
   return (
     <View style={styles.container}>
         <View style={styles.profileContainer}>
             <Image source={profileImage} style={styles.profileImage}/>
-            <Text style={styles.profileName}>Narongrit Prommadaj</Text>
+            <View>
+                <Text style={styles.profileName}>{name}</Text>
+                <Button title="Click Me" onPress={handleChangeName}/>
+                <Text></Text>
+                <Button title="Click Me" onPress={handleChangeImahe}/>
+            </View>
         </View>
     </View>
   )
