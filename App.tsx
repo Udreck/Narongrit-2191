e1a56815a5831dff76cca2daba1b4bd315407340
Udreck <narongrit_st@tni.ac.th>
2024-09-07 09:23:09 +0700

    ex24/8

--- App.tsx
+++ App.tsx
@@ -4,6 +4,8 @@ import ProfileScreen from './componant/ProfileScreen'
 import FlashListExample from './componant/FlashListExample'
 import FlashListcallbackend from './componant/FlashListcallbackend'
 import NewsApp from './componant/NewsApp'
+import WeatherLondon from './componant/WeatherLondon'
+import WeatherBangkok from './componant/WeatherBangkok'
 
 
 const App = ():React.JSX.Element => {
@@ -12,7 +14,9 @@ const App = ():React.JSX.Element => {
       {/*<ProfileScreen/>*/}
       {/*<FlashListExample/>*/}
       {/*<FlashListcallbackend/>*/}
-      {<NewsApp/>}
+      {/* <NewsApp/> */}
+      {/* <WeatherLondon/> */}
+      <WeatherBangkok/>
     </View>
   )
 }
